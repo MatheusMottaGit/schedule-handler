17c206eec12f90277c02397a6aecafd3e5017320
Matheus Motta <matheusdomingues423@gmail.com>
2024-05-24 13:10:36 -0300

    chat completion actually working

--- src/handlers/schedule_handler.py
+++ src/handlers/schedule_handler.py
@@ -11,14 +11,7 @@ class ScheduleHandler:
     page = reader.pages[0]
     content = page.extract_text()
     return content
-
-  def set_gpt_connection():
-    client = OpenAI(
-      api_key=os.environ.get('OPENAI_API_KEY')
-    )
-
-    return client
-
+  
   def generate_schedule(self):
     names = [
       'Matheus',
@@ -38,7 +31,7 @@ class ScheduleHandler:
     ]
 
     schedule_template = """
-      Escala do mês {nome do mês que esta no conteúdo do arquivo} (utilize CAPS LOCK)
+      Escala do mês da comunidade São José {nome do mês que esta no conteúdo do arquivo} (utilize CAPS LOCK)
 
       - {dia do mês} ({horário da missa}) 
         - Nome 1
@@ -48,18 +41,25 @@ class ScheduleHandler:
 
     file_content = self.handle_pdf_content()
 
-    openai = self.set_gpt_connection()
+    openai = OpenAI(
+      api_key=os.environ.get('OPENAI_API_KEY')
+    )
 
     chat_completion = openai.chat.completions.create(
       messages=[
-        {"role": "user", "content": "Se comporte como um coordenador da comunidade São José que deve mexer em uma escala de missas e/ou compromissos do mês. Você deve organizar, em uma lista, servidores que devem ir à comunidade nos dias de missa."},
-        {"role": "user", "content": f"A lista deve ser montada com os seguintes nomes, alías, APENAS estes nomes e nada mais a acrescentar: { names }"},
+        {"role": "user", "content": "Se comporte como um coordenador da comunidade **São José** que deve mexer em uma escala de missas e/ou compromissos do mês. Você deve organizar, em uma lista APENAS DA COMUNIDADE SÃO JOSÉ, servidores que devem ir nos dias de missa."},
+        {"role": "user", "content": "Analise o conteúdo, e separe os dias APENAS DA COMUNIDADE SÃO JOSÉ, e nenhuma comunidade mais."},
+        {"role": "user", "content": f"A lista deve ser montada com os seguintes nomes, aliás, APENAS estes nomes e nada mais a acrescentar: { names }"},
+        {"role": "user", "content": f"O Matheus DEVE estar em todos os sábados APENAS, e o Caio e Rillary podem estar nas quartas-feiras. A Valentina pode estar tanto nos sábados, quanto nas quartas."},
         {"role": "user", "content": f"Dados os nomes, a escala deve manter o seguinte modelo com, NO MÁXIMO, 5 nomes por dia de missa: { schedule_template }"},
-        {"role": "user", "content": f"Dadas as instruções, segue o conteúdo do arquivo em que você vai se basear: { file_content }"},
+        {"role": "user", "content": f"Dadas as instruções, as quais devem ser seguidas à risco, segue o conteúdo do arquivo em que você vai se basear: { file_content }"},
       ],
       model="gpt-3.5-turbo"
     )
 
-    return chat_completion
+    response = chat_completion.choices[0].message.content
 
-    # print(chat_completion)
+    print(response)
+
+    with open('schedule.txt', 'w') as file:
+      file.write(response)
